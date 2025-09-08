4fa484c54a14faab9159a4aa9c0cd664043a6ac4
Allen_Yeh <happy1294123@gmail.com>
2025-09-08 09:25:11 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+import { useLiff } from 'react-liff';
+
+import './App.css';
+
+const App = () => {
+	const [displayName, setDisplayName] = useState('');
+	const { error, isLoggedIn, isReady, liff } = useLiff();
+
+	useEffect(() => {
+		if (!isLoggedIn) return;
+
+		(async () => {
+			const profile = await liff.getProfile();
+			setDisplayName(profile.displayName);
+		})();
+	}, [liff, isLoggedIn]);
+
+	const showDisplayName = () => {
+		if (error) return <p>Something is wrong.</p>;
+		if (!isReady) return <p>Loading...</p>;
+
+		if (!isLoggedIn) {
+			return (
+				<button className="App-button" onClick={liff.login}>
+					Login
+				</button>
+			);
+		}
+		return (
+			<>
+				<p>Welcome to the react-liff demo app, {displayName}!</p>
+				<button className="App-button" onClick={liff.logout}>
+					Logout
+				</button>
+			</>
+		);
+	};
+
+	return (
+		<div className="App">
+			<header className="App-header">{showDisplayName()}</header>
+		</div>
+	);
+};
+
+export default App;
