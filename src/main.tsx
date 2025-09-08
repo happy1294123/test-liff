90aacc75e7420358f4216b935c52626731451377
Allen_Yeh <happy1294123@gmail.com>
2025-09-08 09:48:28 +0800

    add line liff id

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { LiffProvider } from 'react-liff';
 import './index.css'
 import App from './App.tsx'
 
+const liffId = import.meta.env.VITE_REACT_APP_LINE_LIFF_ID;
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+	<StrictMode>
+		<LiffProvider liffId={liffId}>
+			{liffId}
+			<App />
+		</LiffProvider>
+	</StrictMode>,
 )
